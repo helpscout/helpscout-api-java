0d6430428c081cb940f0d9d73fca5697b5df6662
Brian Dame <briandame@gmail.com>
2012-10-10 15:07:54 -0400

    Updated formatting

--- src/main/java/net/helpscout/api/model/thread/BaseLineItem.java
+++ src/main/java/net/helpscout/api/model/thread/BaseLineItem.java
@@ -1,50 +1,56 @@
 package net.helpscout.api.model.thread;
 
-import java.util.Calendar;
-
 import net.helpscout.api.cbo.Status;
 import net.helpscout.api.model.ref.MailboxRef;
 import net.helpscout.api.model.ref.PersonRef;
 import net.helpscout.api.model.ref.UserRef;
+
+import java.util.Calendar;
 
 public class BaseLineItem implements LineItem {
 	private UserRef assignedTo;
 	private Status status;
 	private PersonRef createdBy;
 	private Calendar createdAt;
-	
+
 	private MailboxRef fromMailbox;
-	
+
 	public boolean isAssigned() {
 		return this.assignedTo != null;
 	}
+
 	public boolean isActive() {
 		return this.status == Status.Active;
 	}
+
 	public boolean isPending() {
 		return this.status == Status.Pending;
 	}
+
 	public boolean isClosed() {
 		return this.status == Status.Closed;
 	}
+
 	public boolean isSpam() {
 		return this.status == Status.Spam;
 	}
+
 	public UserRef getAssignedTo() {
 		return assignedTo;
 	}
+
 	public Status getStatus() {
 		return status;
 	}
-	
+
 	public boolean isStatusChange() {
 		return status != Status.NoChange;
 	}
-	
+
 	public PersonRef getCreatedBy() {
 		return createdBy;
 	}
-	
+
 	public Calendar getCreatedAt() {
 		return createdAt;
 	}
