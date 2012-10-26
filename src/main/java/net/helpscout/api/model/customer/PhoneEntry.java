3f7ff79c24448ebbf113606fed4f802cf2c5b56f
Brian Dame <briandame@gmail.com>
2012-10-26 16:13:11 -0400

    Added Location enum

--- src/main/java/net/helpscout/api/model/customer/PhoneEntry.java
+++ src/main/java/net/helpscout/api/model/customer/PhoneEntry.java
@@ -1,5 +1,32 @@
 package net.helpscout.api.model.customer;
 
 public class PhoneEntry extends CustomerEntry {
-	
+
+	public static enum Location {
+		Home("home"),
+		Work("work"),
+		Mobile("mobile"),
+		Fax("fax"),
+		Pager("pager"),
+		Other("other");
+
+		private final String label;
+
+		private Location(String label) {
+			this.label = label;
+		}
+
+		public String getLabel() {
+			return this.label;
+		}
+
+		public static Location findByLabel(String label) {
+			for (Location item : Location.values()) {
+				if (item.getLabel().equalsIgnoreCase(label)) {
+					return item;
+				}
+			}
+			return null;
+		}
+	}
 }
