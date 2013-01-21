818f8777bcbd86ee6f38bb7f7fa1b96c5f351517
Brian Dame <briandame@gmail.com>
2013-01-21 11:00:42 -0500

    Added serialize method

--- src/main/java/net/helpscout/api/adapters/ConversationTypeAdapter.java
+++ src/main/java/net/helpscout/api/adapters/ConversationTypeAdapter.java
@@ -1,9 +1,6 @@
 package net.helpscout.api.adapters;
 
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
+import com.google.gson.*;
 import net.helpscout.api.cbo.ConversationType;
 
 import java.lang.reflect.Type;
@@ -13,9 +10,13 @@ import java.lang.reflect.Type;
  *
  * @author briandame@gmail.com
  */
-public class ConversationTypeAdapter implements JsonDeserializer<ConversationType> {
+public class ConversationTypeAdapter implements JsonDeserializer<ConversationType>, JsonSerializer<ConversationType> {
 
 	public ConversationType deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
 		return ConversationType.findByLabel(json.getAsString());
+	}
+
+	public JsonElement serialize(ConversationType type, Type typeOfT, JsonSerializationContext context) {
+		return new JsonPrimitive(type.getLabel());
 	}
 }
